61cd509a1f357e37ee84142ec33c2f62f0957175
Ginger Defino <157766522+gingerdefino@users.noreply.github.com>
2026-03-04 22:48:54 +0000

    added brand colors

--- src/scripts/brandSettings.js
+++ src/scripts/brandSettings.js
@@ -6,14 +6,14 @@ export const brandSettings = {
             { 
                 name: "Primary - Light Background", 
                 src: "", // Add image path
-                bg: "#ffffff", 
+                bg: "#F2EFEB", 
                 darkText: true,
                 description: "Use this version on white or light backgrounds."
             },
             { 
                 name: "Primary - Dark Background", 
                 src: "", // Add image path
-                bg: "#333333", 
+                bg: "#4A1719", 
                 darkText: false,
                 description: "Use this version on dark or brand-colored backgrounds."
             },
@@ -35,8 +35,8 @@ export const brandSettings = {
     },
     favicon: "/favicon.svg", // Add favicon URL here
     typography: {
-        primaryFont: "Helvetica Neue",  // this will automatically get set as the page font
-        secondaryFont: "Verdana", // dont be a bum, go find some google/adobe fonts you like. just make sure to load them in the stylesheet or Layout astro file 
+        primaryFont: "Alexandria",  // this will automatically get set as the page font
+        secondaryFont: "Riant Display", // dont be a bum, go find some google/adobe fonts you like. just make sure to load them in the stylesheet or Layout astro file 
         baseSize: "16px",
         lineHeight: "1.5",
         paragraphSpacing: "2"
@@ -44,10 +44,12 @@ export const brandSettings = {
     palette: [ // these will load automatically into your page into the color section
         { name: "White", hex: "#ffffff" },
         { name: "Black", hex: "#000000" },
-        { name: "Primary Blue", hex: "#0056b3" },
-        { name: "Secondary Teal", hex: "#17a2b8" },
-        { name: "Accent Yellow", hex: "#ffc107" },
-        { name: "Dark Gray", hex: "#343a40" },
-        { name: "Light Gray", hex: "#f8f9fa" }
+        { name: "Primary Maroon", hex: "#4A1719" },
+        { name: "Accent Blue", hex: "#402F8E" },
+        { name: "Light Blue", hex: "#7B77D9" },
+        { name: "Accent Mustard", hex: "#BD9E3D" },
+        { name: "Dark Mustard", hex: "#977718" },
+        { name: "Beige", hex: "#EDE8E2" },
+        { name: "Lighter Beige", hex: "#F2EFEB" }
     ]
 };
